a47645f8ac9934450def0a39f75a3807917b1876
Gilberto Melo <gilbertotads@gmail.com>
2025-04-09 13:20:08 -0300

    adding product component

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../commom/product';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-product-list',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
+
+  products: Product[] = [];
+
+  constructor(private readonly service:ProductService) { }
+
+  ngOnInit(): void {
+   this.listProducts();
+  }
+  listProducts(): void {
+    this.service.getProducts().subscribe({ next: this.setProducts.bind(this) });
+    this.service.getProducts().subscribe({ error: console.info });
+    
+  }
+  setProducts(products: Product[]): void {
+    this.products = products;
+  }
 
 }
